fb13ecc9dc615db03fb48f8a6b9db8265bb41c12
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-08-23 11:23:17 +0800

    Update QDGroups.java

--- src/main/java/io/github/schntgaispock/quantumdungeons/core/slimefun/QDGroups.java
+++ src/main/java/io/github/schntgaispock/quantumdungeons/core/slimefun/QDGroups.java
@@ -7,12 +7,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class QDGroups {
 
-    public static final SubGroup MATERIALS = new SubGroup("materials", QDStacks.GUIDE_ITEM_MATERIALS);
-    public static final SubGroup TOOLS = new SubGroup("tools", QDStacks.GUIDE_ITEM_TOOLS);
-    public static final SubGroup CATALYSTS = new SubGroup("catalysts", QDStacks.GUIDE_ITEM_CATALYSTS);
-    public static final SubGroup RECORDS = new SubGroup("records", QDStacks.GUIDE_ITEM_RECORDS);
-    public static final SubGroup DUNGEON = new SubGroup("dungeon", QDStacks.GUIDE_ITEM_DUNGEON);
-    public static final MultiGroup MAIN = new MultiGroup("main", QDStacks.GUIDE_ITEM_MAIN,
+    public static final SubGroup MATERIALS = new SubGroup("材料", QDStacks.GUIDE_ITEM_MATERIALS);
+    public static final SubGroup TOOLS = new SubGroup("工具", QDStacks.GUIDE_ITEM_TOOLS);
+    public static final SubGroup CATALYSTS = new SubGroup("转换器", QDStacks.GUIDE_ITEM_CATALYSTS);
+    public static final SubGroup RECORDS = new SubGroup("记录", QDStacks.GUIDE_ITEM_RECORDS);
+    public static final SubGroup DUNGEON = new SubGroup("地下城", QDStacks.GUIDE_ITEM_DUNGEON);
+    public static final MultiGroup MAIN = new MultiGroup("主要", QDStacks.GUIDE_ITEM_MAIN,
         MATERIALS, TOOLS, CATALYSTS, RECORDS, DUNGEON);
 
 }
